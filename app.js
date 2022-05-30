a4b01374385019fcf1debd371fa651a421dbb00e
Kornull <wolod1987kor@gmail.com>
2022-05-31 01:40:28 +0300

    fix: tag classname

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='list__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="list__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="btn__edit";
